952bc40ce14024e4c17fa02f50e9c5c537865b28
yalu <114964251+ya1u@users.noreply.github.com>
2022-12-19 17:00:45 +0900

    testtt

--- src/main/java/com/cos/dysson/repository/CartRepository.java
+++ src/main/java/com/cos/dysson/repository/CartRepository.java
@@ -1,5 +1,7 @@
 package com.cos.dysson.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cos.dysson.model.Cart;
@@ -7,5 +9,7 @@ import com.cos.dysson.model.Cart;
 public interface CartRepository extends JpaRepository<Cart, Integer> {
 
 	Cart findByUsersId(int id);
+	
+	List<Cart> findAll(Cart userCart);
 
 }
