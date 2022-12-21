6448b517cfd3b49bfd4eaeb5e28359fab6208495
yalu <114964251+ya1u@users.noreply.github.com>
2022-12-21 16:34:06 +0900

    Revert "test"
    This reverts commit 7ac73a63a8c7ec9a820ea7d036c612ca7f0e22d6.

---
+++ target/classes/static/js/board.js
@@ -0,0 +1,137 @@
+let index = {
+	init: function() {
+		$("#btn-save").on("click",()=>{
+			this.save();
+			
+		}),
+		$("#btn-delete").on("click",()=>{
+			this.deleteById();
+		});
+		$("#btn-update").on("click",()=>{
+			this.update();
+		});
+		
+		$("#btn-replySave").on("click",()=>{
+			this.replySave();
+			
+		})
+/*		$("#btn-replyDelete").on("click",()=>{
+			this.replySave();
+			
+		})*/
+		
+	},
+	save: function() {
+		let data = {
+			secret: $("#secret").val(),
+			category: $("#category").val(),
+			title: $("#title").val(),
+			content: $("#content").val()
+		};
+		$.ajax({ 
+			type:"POST",
+			url:"/api/board",
+			data:JSON.stringify(data), 
+			contentType:"application/json; charset=utf-8",
+			dataType:"json" 
+		}).done(function(resp){
+			alert("글쓰기가 완료되었습니다.");
+			location.href="/auth/support";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+	},
+	update: function(){
+		let id=$("#id").val();
+		
+		let data={
+			title: $("#title").val(),
+			content: $("#content").val()
+		};
+		console.log(id);
+		console.log(data);
+		$.ajax({ 
+			type:"PUT",
+			url:"/api/board/"+id,
+			data:JSON.stringify(data), 
+			contentType:"application/json; charset=utf-8",
+			dataType:"json" 
+		}).done(function(resp){
+			alert("수정이 완료되었습니다.");
+			location.href="/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+	},
+	
+	
+	deleteById: function(){
+		let id=$("#id").text();
+		
+		$.ajax({ 
+			type:"DELETE",
+			url:"/api/board/"+id,
+			dataType:"json" 
+		}).done(function(resp){
+			alert("삭제가 완료되었습니다.");
+			location.href="/auth/support";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+	},
+	
+	replyDelete: function(boardId,replyId) {
+
+		$.ajax({
+			type:"DELETE",
+			url:"/api/board/${boardId}/reply/${replyId}",
+			contentType:"application/json; charset=utf-8",
+			dataType:"json"
+		}).done(function(resp){
+			alert("댓글삭제 성공.");
+			location.href="/board/${boardId}";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+	},
+	
+/*	deleteById: function(){
+		let id=$("#id").text();
+		
+		$.ajax({ 
+			type:"DELETE",
+			url:"/api/board/"+id,
+			dataType:"json" 
+		}).done(function(resp){
+			alert("삭제가 완료되었습니다.");
+			location.href="/auth/support";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+	},*/
+	replySave:function(){
+		let data={
+			usersId: $("#usersId").val(),
+			boardsId: $("#boardsId").val(),
+			content: $("#reply-content").val()
+		};
+		
+		
+		$.ajax({ 
+			type:"POST",
+			url:`/api/board/${data.boardsId}/reply`,
+			data:JSON.stringify(data),
+			contentType:"application/json; charset=utf-8",
+			dataType:"json" 
+		}).done(function(resp){
+			/*alert("댓글 작성이 완료되었습니다.");*/
+			location.href=`/board/${data.boardsId}`;
+
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+
+		});
+	},
+	
+}
+index.init();
---
+++ target/classes/static/js/product.js
@@ -0,0 +1,69 @@
+let index={
+	init: function() {
+		$("#btn-add").on("click",()=>{
+			this.add();
+		});
+		$("btn-update").on("click",()=>{
+			this.update();
+		});
+	},
+	add: function() {
+		let data = {
+			name: $("#name").val(),
+			content: $("#content").val(),
+			made: $("#made").val(),
+			price: $("#price").val(),
+			category: $("#category").val(),
+			img: $("#img").val()
+		};
+		$.ajax({
+			type: "POST",
+			url: "/api/product",
+			data: JSON.stringify(data),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json"
+		}).done(function(resp){
+			alert("제품 등록이 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error){
+			alert(JSON.stringify(error))
+		});
+	},
+	deleteById: function(id) {
+		console.log(id);
+		$.ajax({
+			type: "DELETE",
+			url: `/api/product/${id}`,
+			dataType: "json"
+		}).done(function(resp) {
+			alert("제품이 삭제되었습니다.");
+			location.href = "/product/store";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+	},
+	update: function() {
+		let id = $("#id").val();
+		
+		let data = {
+			name: $("#name").val(),
+			content: $("#content").val(),
+			made: $("#made").val(),
+			price: $("#price").val(),
+			category: $("#category").val()
+		};
+		$.ajax({
+			type: "PUT",
+			url: "/api/product/" + id,
+			data: JSON.stringify(data),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json"
+		}).done(function(resp) {
+			alert("수정이 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+	},
+}
+index.init();
